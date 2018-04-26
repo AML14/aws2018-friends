1c7d55177d9c2b844d009fce41d2f235fe3aebea
Manuel Resinas <resinas@us.es>
2018-04-26 21:05:45 +0200

    Update contactsResource.js

--- contactsResource.js
+++ contactsResource.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var urljoin = require('url-join');
-var contactsServer = (process.env.CONTACTS_URL || 'https://aws2018-staging-mr.herokuapp.com/api/v1');
+var contactsServer = (process.env.CONTACTS_URL || 'https://aws2018-staging.herokuapp.com/api/v1');
 var contactsKey = '1234abcd';
 
 module.exports = function(url) {
