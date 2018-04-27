f187d5962a63e6777ea9e2a7b4e15b947ffc9223
Manuel Resinas <resinas@us.es>
2018-04-27 08:25:13 +0000

    Updated to 2018 version

--- contactsResource.js
+++ contactsResource.js
@@ -2,7 +2,7 @@
 
 var urljoin = require('url-join');
 var contactsServer = (process.env.CONTACTS_URL || 'https://aws2018-staging.herokuapp.com/api/v1');
-var contactsKey = '1234abcd';
+var contactsKey = 'asdfgasdf';
 
 module.exports = function(url) {
     return urljoin(contactsServer, url, '?apikey='+contactsKey);
--- public/list-ctrl.js
+++ public/list-ctrl.js
@@ -1,7 +1,7 @@
 angular.module("ContactListApp").controller("ListCtrl", function($scope,$http) {
 
     function refresh(){
-        $http.get("https://aws2017-staging.herokuapp.com/api/v1/contacts?apikey=1234abcd").then(function (response){
+        $http.get("https://aws2018-staging.herokuapp.com/api/v1/contacts?apikey=asdfg").then(function (response){
             $scope.contacts = response.data;
         });
     }
